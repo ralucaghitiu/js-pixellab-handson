73d04a586cff49d6e265bdf4f746f8c75151d2b1
ralucaghitiu <b.ralucaelena@gmail.com>
2022-06-17 00:05:48 +0300

    tema -part2- S06-EX02

--- M01S06/ex02/app.js
+++ M01S06/ex02/app.js
@@ -1,11 +1,28 @@
 const paragraphElement = document.createElement('p');
 const userAgentString = navigator.userAgent;
 const messsage = 'Nu stim ce browser folosesti';
-
-if (userAgentString.includes('chrome')) {
-  messsage = 'Navighez folosing Chrome';
-}
+const browserName = '';
 
 paragraphElement.innerText = messsage;
 
 document.body.append(paragraphElement);
+
+// tema
+
+if (userAgentString.includes('firefox')) {
+  messsage = 'Navighez folosind Firefox';
+}
+
+setTimeout(() => {
+  const userName = prompt(`${browserName} vrea sa stie cum te cheama.`);
+  const headingElement = document.createElement('h1');
+  const message = `Salut ${userName}!`;
+
+  headingElement.innerText = message;
+
+  document.body.append(headingElement);
+
+  setTimeout(() => {
+    paragraphElement.remove();
+  }, 3000);
+}, 3000);
