92a9a898ee6edf41f4232690ab1ff119c4b8d673
ralucaghitiu <b.ralucaelena@gmail.com>
2022-07-21 14:43:15 +0300

    Tema - M02S04 -ex03

--- M02S04/ex03/app.js
+++ M02S04/ex03/app.js
@@ -8,18 +8,45 @@ const axis = {
   S: 'y',
   W: 'x',
   E: 'x',
+  NW: 'x',
+  NE: 'x',
+  SE: 'x',
+  SW: 'x',
 };
 const direction = {
   N: 'back',
   S: 'fw',
   W: 'back',
   E: 'fw',
+  NW: 'back',
+  NE: 'fw',
+  SE: 'fw',
+  SW: 'back',
   ArrowDown: 'fw',
   ArrowUp: 'back',
+  ArrowRight: 'fw',
+  ArrowLeft: 'back',
 };
+
+const secondAxis = {
+  NW: 'y',
+  SE: 'y',
+  NE: 'y',
+  SW: 'y',
+};
+
+const secondDirection = {
+  NW: 'back',
+  SE: 'fw',
+  SW: 'fw',
+  NE: 'back',
+};
+
 const arrowMap = {
   ArrowDown: 'y',
   ArrowUp: 'y',
+  ArrowLeft: 'x',
+  ArrowRight: 'x',
 };
 const hero = {
   x: 0,
@@ -39,10 +66,17 @@ controls.addEventListener('click', function (event) {
     return;
   }
   const value = target.className;
-  const currentAxis = axis[value];
-  const currentDirection = direction[value];
+  const currentAxis1 = axis[value];
+  const currentAxis2 = secondAxis[value];
+  const currentDirection1 = direction[value];
+  const currentDirection2 = secondDirection[value];
 
-  updateHeroPosition(currentAxis, currentDirection);
+  updateHeroPosition(
+    currentAxis1,
+    currentDirection1,
+    currentAxis2,
+    currentDirection2,
+  );
 
   renderHero(hero);
 });
@@ -62,12 +96,22 @@ document.addEventListener('keydown', (event) => {
   renderHero(hero);
 });
 
-function updateHeroPosition(currentAxis, currentDirection) {
+function updateHeroPosition(
+  currentAxis1,
+  currentDirection1,
+  currentAxis2,
+  currentDirection2,
+) {
   // ternary operator
-  hero[currentAxis] =
-    currentDirection === 'back'
-      ? hero[currentAxis] - step
-      : hero[currentAxis] + step;
+  hero[currentAxis1] =
+    currentDirection1 === 'back'
+      ? hero[currentAxis1] - step
+      : hero[currentAxis1] + step;
+
+  hero[currentAxis2] =
+    currentDirection2 === 'back'
+      ? hero[currentAxis2] - step
+      : hero[currentAxis2] + step;
 
   // simplify
   if (hero.x <= 0) {
@@ -89,8 +133,8 @@ function updateHeroPosition(currentAxis, currentDirection) {
 
 // hoisting
 function renderHero(hero) {
-  //   heroElement.style.left = `${hero.x}px`;
-  //   heroElement.style.top = `${hero.y}px`;
+  // heroElement.style.left = `${hero.x}px`;
+  // heroElement.style.top = `${hero.y}px`;
 
   const cssText = `transform: translate(${hero.x}px, ${hero.y}px)`;
   heroElement.setAttribute('style', cssText);
